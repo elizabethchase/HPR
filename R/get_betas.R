fe547e42b778d675d4be2a719ccfd922d27b60aa
Elizabeth Chase <ecchase@umich.edu>
2022-11-06 20:21:43 -0500

    bug

--- R/get_betas.R
+++ R/get_betas.R
@@ -36,15 +36,15 @@ get_betas <- function(object = NULL,
   has_covs <- object$data$has_covs
   p <- object$data$p
   scales <- object$scale_sd
-  cont_preds <- c(1:p)[-object$bin_preds]
+  cont_preds <- c(1:p)[-object$bin_pred]
   if (has_covs==0){stop("This model object has no linear coefficients.")}
   lower <- alpha/2
   upper <- 1-(alpha/2)
   if (is.null(var_names)){var_names <- 1:p}
 
   my_summary <- dplyr::select(as_draws_df(object$stan_object$draws("beta")), -.iteration, -.chain, -.draw)
-  for(i in 1:p){
-      my_summary[,cont_preds[i]] <- my_summary[,cont_preds[i]]*scales[i]
+  for(i in 1:length(cont_preds)){
+      my_summary[,cont_preds[i]] <- my_summary[,cont_preds[i]]/scales[i]
   }
 
   if (family=="gaussian"){
